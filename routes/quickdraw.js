0c39868961558f3323c01761665458de3f93bb38
Tomo ABE <tomhid@users.noreply.github.com>
2017-09-20 18:36:27 +0900

    Add files via upload

--- routes/quickdraw.js
+++ routes/quickdraw.js
@@ -7,4 +7,28 @@ router.get('/', function(req, res, next) {
   res.send('quickdraw');
 });
 
+router.post('/', function(req, res, next) {
+//  res.render('index', { title: 'Express' });
+//console.log(req.body);
+var data = req.body.imagedata.split(',');
+var b64img = data[ 1 ];
+b64img2 = b64img.replace(/ /g, '+');
+//console.log(b64img2);
+//console.log(b64img.length);
+//console.log(b64img2.length);
+//var base64 = require('urlsafe-base64');
+//var img = base64.decode( b64img );
+//var fs = require('fs');
+//fs.writeFile('sample.png', img, function (err) {
+//    console.log(err);
+//});
+fs.writeFile("sample2.png", new Buffer(b64img2, "base64"), function(err) {});
+//fs.writeFile("sample.txt", b64img.toString(), function(err) {});
+//fs.writeFile("sample2.txt", b64img2.toString(), function(err) {});
+
+  res.send('finish');
+});
+
+
+
 module.exports = router;
